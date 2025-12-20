408706c1f7a1a3096865f9d6967474bdb397d168
normalman743 <128381694+normalman743@users.noreply.github.com>
2025-12-20 22:03:37 +0800

    move big file to untracked folders

--- websocket_manager.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility shim for the legacy websocket manager import path."""
-
-from app.services.websocket_manager import ConnectionManager, manager
-
-__all__ = ["ConnectionManager", "manager"]
